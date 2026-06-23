710ccd361d6cd3b8dd342a507e87165bfbcff4f2
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2026-06-23 22:25:08 +0800

    Add Input Component

---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,38 @@
+import cx from "classnames";
+import styles from "./Input.module.css";
+
+type InputProperties = {
+  label?: string;
+  value: string;
+  onChange: (newValue: string) => void;
+  placeholder?: string;
+  type?: "text" | "password" | "email" | "number" | "date" | "tel";
+  useTranslucentBackground?: boolean;
+  className?: string;
+};
+
+export default function Input({
+  label,
+  value,
+  onChange,
+  placeholder,
+  type = "text",
+  useTranslucentBackground = false,
+  className,
+}: InputProperties) {
+  return (
+    <div className={styles.group}>
+      <label className={styles.label}>{label}</label>
+      <input
+        className={cx(styles.label, {
+          [styles.transparent]: useTranslucentBackground,
+          className,
+        })}
+        value={value}
+        type={type}
+        placeholder={placeholder}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
